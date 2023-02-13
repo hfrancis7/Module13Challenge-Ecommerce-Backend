b4db3ea91d2b0ee049254f90092df39621df2fd8
Hailey Francis <n01402670@unf.edu>
2023-02-13 15:53:25 -0500

    productTag model done

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false //unsure
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false //unsure
+      }
+    }
   },
   {
     sequelize,
