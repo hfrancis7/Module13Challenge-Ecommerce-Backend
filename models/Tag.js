94f7952586d30c41701076397d89b267f9523be4
Hailey Francis <n01402670@unf.edu>
2023-02-13 15:49:44 -0500

    tag model done

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      //does not say to disallow NULL in README.md
+    }
   },
   {
     sequelize,
