f46b098988f0843ecc1126dc8e36098e3be1e70a
Hailey Francis <n01402670@unf.edu>
2023-02-13 16:31:37 -0500

    added through table for productTag

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unqiue: false
+  },
+  as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagged_products'
+})
 
 module.exports = {
   Product,
